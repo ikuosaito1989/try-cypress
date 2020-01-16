78452b534d16c1f8178417763dd430847e500f66
ikuo saito <ikuosaito1989@gmail.com>
2020-01-16 16:53:52 +0900

    Step 1: Visit a page ref. https://docs.cypress.io/guides/getting-started/writing-your-first-test.html#Step-1-Visit-a-page

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,5 +1,8 @@
 describe('My First Test', function() {
   it('Does not do much!', function() {
-    expect(true).to.equal(false)
+    expect(true).to.equal(true)
+  })
+  it('Visits the My Profile', function() {
+    cy.visit('https://festive-yonath-a04e1e.netlify.com/')
   })
 })
