e8b7f38905113b02cfb2e4cdd9c04f79a32ace0b
ikuo saito <ikuosaito1989@gmail.com>
2020-01-17 10:57:42 +0900

    アロー関数に変更

--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -1,16 +1,16 @@
 /// <reference types="Cypress" />
-describe('プロフィールページ', function() {
-  it('ラベルが表示されること', function() {
+describe('プロフィールページ', () => {
+  it('ラベルが表示されること', () =>  {
     cy.visit('https://festive-yonath-a04e1e.netlify.com/')
     cy.contains('みんなの洋楽ランキング')
   })
 
-  it('ラベルにクリック出来ること', function() {
+  it('ラベルにクリック出来ること', () =>  {
     cy.visit('https://festive-yonath-a04e1e.netlify.com/')
     cy.contains('みんなの洋楽ランキング').click()
   })
 
-  it('ポートフォリオにクリックし、詳細画面へ遷移出来ること', function() {
+  it('ポートフォリオにクリックし、詳細画面へ遷移出来ること', () =>  {
     cy.visit('https://festive-yonath-a04e1e.netlify.com/')
     cy.get('.portfolio-item-caption').first().click()
     cy.url().should('include', '/portfolio/1')
