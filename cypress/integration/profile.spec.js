d1b51100114c37740f4d65abb87069cbf1dec333
ikuo saito <ikuosaito1989@gmail.com>
2020-01-17 10:50:11 +0900

    Step 4: Make an assertion ref. https://docs.cypress.io/guides/getting-started/writing-your-first-test.html#Step-4-Make-an-assertion

--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -8,4 +8,15 @@ describe('My First Test', function() {
     cy.visit('https://festive-yonath-a04e1e.netlify.com/')
     cy.contains('みんなの洋楽ランキング').click()
   })
+  it('clicking "みんなの洋楽ランキング" navigates to a new url', function() {
+    cy.visit('https://festive-yonath-a04e1e.netlify.com/')
+    
+    // 先頭の要素がクリックされ、/portfolio/1する
+    cy.get('.portfolio-item-caption').first().click()
+    cy.url().should('include', '/portfolio/1')
+
+    // 全ての要素をクリック。最後のアサーションのみ評価される（つまり/portfolio/1）は評価されない
+    // cy.get('.portfolio-item-caption').click({ multiple: true })
+    // cy.url().should('include', '/portfolio/4')
+  })
 })
--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -12,4 +12,12 @@ describe('My First Test', function() {
 
     cy.contains('type').click()
   })
+  it('clicking "type" navigates to a new url', function() {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+  })
 })
